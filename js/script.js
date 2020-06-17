0799bb282832e76a1f74cde752b8166443c94d19
Andrea <miranda.andrea.nd@gmail.com>
2020-06-17 16:52:11 +0200

    parte 1 js con nome, km e età

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+var creaButton = document.getElementById("creaBiglietto")
+
+creaButton.addEventListener("click", function(){
+  var nome = document.getElementById("nome").value;
+  var km = document.getElementById("km").value;
+  var categoria = document.getElementById("categoria").value;
+  //  test console
+  console.log(nome);
+  console.log(km);
+  console.log(categoria);
+
+})
